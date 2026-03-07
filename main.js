a6a8edb01bba899b76115496894245b21d28c860
muhammadSalmanSakib <sakibsalmanh@gmail.com>
2026-03-08 05:50:25 +0600

    added main js code

---
+++ main.js
@@ -0,0 +1,20 @@
+const issuesContainer = document.getElementById("issuesContainer");
+const issueCount = document.getElementById("issueCount");
+const spinner = document.getElementById("spinner");
+const emptyState = document.getElementById("emptyState");
+
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+
+const allTabs = document.querySelectorAll("[data-tab]");
+
+const issueModal = document.getElementById("issueModal");
+const modalTitle = document.getElementById("modalTitle");
+const modalStatus = document.getElementById("modalStatus");
+const modalCategory = document.getElementById("modalCategory");
+const modalPriority = document.getElementById("modalPriority");
+const modalDescription = document.getElementById("modalDescription");
+const modalAuthor = document.getElementById("modalAuthor");
+const modalCreatedAt = document.getElementById("modalCreatedAt");
+const modalLabel = document.getElementById("modalLabel");
+const modalPriorityText = document.getElementById("modalPriorityText");
